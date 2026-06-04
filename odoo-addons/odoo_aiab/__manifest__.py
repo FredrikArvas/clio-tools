1bacb5413ba583a2351968fedf4fc85c18f5f2ab
Fredrik Arvas <fredrik@arvas.se>
2026-06-04 18:22:37 +0200

    feat(odoo_aiab): lägg till 7 moduler i installationsprofilen
    Tillagda: clio_event_log, clio_interview, clio_mail_permissions,
    clio_ncc_project, l10n_se_partner, partner_firstname, partner_multi_relation

--- odoo-addons/odoo_aiab/__manifest__.py
+++ odoo-addons/odoo_aiab/__manifest__.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'AIAB — Installationsprofil',
-    'version': '19.0.4.0.0',
+    'version': '19.0.4.1.0',
     'summary': 'Meta-modul: installerar alla moduler för AIAB-databasen',
     'author': 'Arvas International AB',
     'license': 'LGPL-3',
     'depends': [
-        'clio_cockpit',        # Dashboard och startvy
-        'clio_discuss',        # Diskussioner och meddelandefunktioner
-        'clio_graph',          # Relationsgrafer mellan kontakter
-        'clio_job',            # Rekrytering och tjänster
-        'clio_mail_admin',     # E-postadministration
-        'clio_theme',          # AIAB-färgtema
-        'clio_vigil',          # Bevakning och uppföljning
-        'l10n_se_ssn',         # Personnummer på kontakter
-        'partner_autocomplete', # Företagssökning via VAT/org.nr (Odoo standard)
+        'clio_cockpit',          # Dashboard och startvy
+        'clio_discuss',          # Diskussioner och meddelandefunktioner
+        'clio_event_log',        # Händelselogg
+        'clio_graph',            # Relationsgrafer mellan kontakter
+        'clio_interview',        # Intervjuer och anställningsprocess
+        'clio_job',              # Rekrytering och tjänster
+        'clio_mail_admin',       # E-postadministration
+        'clio_mail_permissions', # Behörighetsstyrning för e-post
+        'clio_ncc_project',      # NCC-projektkort i Odoo
+        'clio_theme',            # AIAB-färgtema
+        'clio_vigil',            # Bevakning och uppföljning
+        'l10n_se_partner',       # Svenska adressformat och fält
+        'l10n_se_ssn',           # Personnummer på kontakter
+        'partner_autocomplete',  # Företagssökning via VAT/org.nr (Odoo standard)
+        'partner_firstname',     # Förnamn och efternamn som separata fält (OCA)
+        'partner_multi_relation', # Relationstyper mellan kontakter (OCA)
     ],
     'post_init_hook': 'post_init_hook',
     'installable': True,
