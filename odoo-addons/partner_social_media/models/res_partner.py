69bdf4bf0351082c187464092999eea998d9eec9
Fredrik Arvas <fredrik@arvas.se>
2026-06-18 07:29:40 +0200

    partner_social_media: add tracking=True to all social fields

--- odoo-addons/partner_social_media/models/res_partner.py
+++ odoo-addons/partner_social_media/models/res_partner.py
@@ -4,8 +4,8 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    social_linkedin = fields.Char('LinkedIn')
-    social_twitter = fields.Char('X / Twitter')
-    social_facebook = fields.Char('Facebook')
-    social_instagram = fields.Char('Instagram')
-    social_github = fields.Char('GitHub')
+    social_linkedin = fields.Char('LinkedIn', tracking=True)
+    social_twitter = fields.Char('X / Twitter', tracking=True)
+    social_facebook = fields.Char('Facebook', tracking=True)
+    social_instagram = fields.Char('Instagram', tracking=True)
+    social_github = fields.Char('GitHub', tracking=True)
